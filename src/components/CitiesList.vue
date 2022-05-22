2c19cd04cbebf61c241e1179ce1172f3715f71f0
s0wie <sowieh@gmail.com>
2022-05-23 00:14:31 +0200

    added pokemon api

---
+++ src/components/CitiesList.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+  created() {
+    fetch("https://pokeapi.co/api/v2/pokemon")
+      .then((response) => response.json())
+      .then((pokemons) => {
+        this.pokemons = pokemons.results;
+      });
+  },
+  data() {
+    return {
+      pokemons: null,
+    };
+  },
+};
+</script>
+
+<template>
+  <ol>
+    <li v-for="pokemon in pokemons">{{ pokemon.name }}</li>
+  </ol>
+</template>
